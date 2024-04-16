49bc334bbb2c7de3be297c377085040f29e95379
moramaan <dmorahidalgo@gmail.com>
2024-04-16 16:23:58 +0200

    Solve UUIDs find problems

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,20 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     protected $fillable = [
+        // 'id',
         'item_id',
         'buyer_id',
         'seller_id',
         'price',
         'transaction_date',
     ];
+
+    protected $guarded = [
+        'id',
+        'admin',
+        'created_at',
+        'updated_at',
+    ];
+
+    // protected $casts = [
+    //     'transaction_date' => 'datetime:Y-m-d',
+    // ];
 
     public function item()
     {
